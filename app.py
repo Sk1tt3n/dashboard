2e7919679f59fb9c8871ef2078b929a2596a21f8
Angelina <angelinahuynh007@gmail.com>
2021-05-10 18:48:05 -0400

    screwed up branch oh no :(

--- app.py
+++ app.py
@@ -15,15 +15,4 @@ def index():
     users = [ 'Rosalia','Adrianna','Victoria','Mihad','Angelina' ]
     return render_template('index.html', title='Welcome', members=users)
 
-@app.route('/instances')
-def instances():
-   ec2_resource = boto3.client('ec2')
-   response = ec2_resource.describe_instances()
-   instances = []
-   for reservation in response['Reservations']:
-    for instance in reservation['Instances']:
-      instances.append(f"Instance Id: {instance['InstanceId']} | Instance Type: {instance['InstanceType']} | Monitoring State: {instance['Monitoring']['State']}")
-   obj = {'Instances': instances}
-   return render_template('instances.html', title='EC2 Instances', instances=obj)
-
 app.run(host='0.0.0.0', port=81)
